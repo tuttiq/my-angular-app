10b7fe98b13b08bb876c3c998990808aa62823ed
Tutti Quintella <tuane.quintella@quipper.com>
2018-08-19 18:10:08 +0900

    add messages service and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms'; // needed for NgModel data binding
 import { AppComponent } from './app.component';
 import { ItemsComponent } from './items/items.component';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemsComponent,
-    ItemDetailComponent
+    ItemDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/items.service.ts
+++ src/app/items.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+
+import { MessageService } from './message.service';
 import { Item } from './item';
 import { ITEMS } from './mock-data';
 
@@ -7,9 +9,11 @@ import { ITEMS } from './mock-data';
   providedIn: 'root'
 })
 export class ItemsService {
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getItems(): Observable<Item[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(ITEMS);
   }
 }
