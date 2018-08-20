70d2aa65eb76b6f1fc3b871d7d39e5f2ce2da0c2
Tutti Quintella <tuane.quintella@quipper.com>
2018-08-21 01:24:06 +0900

    cleanup for educational purposes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // needed for NgModel data binding
 
 import { AppComponent } from './app.component';
-import { ItemsComponent } from './items/items.component';
-import { ItemDetailComponent } from './item-detail/item-detail.component';
 import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemsComponent,
-    ItemDetailComponent,
     MessagesComponent,
     DashboardComponent
   ],
