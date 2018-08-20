5548f992d96cbcd7f486df366a7fa844a40f8556
Tutti Quintella <tuane.quintella@quipper.com>
2018-08-21 01:06:22 +0900

    break messages for educational purposes

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -7,10 +7,20 @@ import { MessageService } from '../message.service';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
+  messages: string[];
 
-  constructor(public messageService: MessageService) { }
+  constructor(private messageService: MessageService) { }
 
   ngOnInit() {
+    this.getMessages();
+  }
+
+  getMessages(): void {
+    this.messages = this.messageService.messages
+  }
+
+  clearMessages(): void {
+    this.messageService.clear();
   }
 
 }
